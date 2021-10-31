e1211abf4e3c68fa738ec9c6fe5947298efd9941
Ant0n101408 <antoni.delfin@vp.pl>
2021-10-31 20:50:02 +0100

    Add lab 4

---
+++ zad_1.py
@@ -0,0 +1,17 @@
+# Zadanie 1
+class Student:
+    def __init__(self, name: str, marks: int ):
+        self.name=name
+        self.marks=marks
+
+    def __str__(self) -> str:
+        return f'Name:{self.name} Marks:{self.marks}'
+
+    @property
+    def is_passed(self) -> bool:
+        return self.marks>50
+
+student1=Student('Antoni',60)
+student2=Student('Dawid',60)
+print(student1.is_passed)
+print(student2.is_passed)
